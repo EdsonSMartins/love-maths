b6717c592c194441607f8c2625f4a699bbe97e7f
EdsonSMartins <smartins.edson@gmail.com>
2023-05-22 19:33:16 +0000

    Add favicon, code for division game, and final tyde ups for the user

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition")
 })
 
@@ -23,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -34,6 +43,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if(gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if(gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -49,7 +60,7 @@ function runGame(gameType) {
 
 function checkAnswer() {
     
-    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let userAnswer = parseFloat(document.getElementById("answer-box").value);
     let calculatedAnswer = calculatedCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
 
@@ -81,6 +92,8 @@ function calculatedCorrectAnswer(){
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert('Unimplemented operator ${operator}');
         throw 'Unimplemented operator ${operator}. Aborting!';
@@ -125,6 +138,8 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDivisionQuestion(){
-
+function displayDivisionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
